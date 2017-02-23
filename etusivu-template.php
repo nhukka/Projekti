f0045adf1779a6143a2467a8772c172a90e8ef37
nhukka <nea.hukka@gmail.com>
2017-02-23 13:54:23 +0200

    changing something random

--- etusivu-template.php
+++ etusivu-template.php
@@ -3,12 +3,12 @@
 <div class="content-row">
     <img src="http://localhost/wordpress/wp-content/themes/Projekti/img/LogoKK_pieni.png">
    
-    
     <div class="kaupungit">
         
         <h1 class="hel">HELSINKI</h1>
         <div class="hpiilo">
-            <button id="moveleft">Move Left</button>  <button id="moveright">Move right</button>
+            <button id="moveleft">Move Left</button>
+            <button id="moveright">Move right</button>
                 <?php
                     $the_query = new WP_Query( array( 'tag_id' => 8 ) );
                     if ( $the_query->have_posts() ) {
@@ -27,8 +27,8 @@
             
         </div>
         <h1 class="esp">ESPOO</h1>
-        <div class="epiilo">
-        <?php
+            <div class="epiilo">
+                <?php
                     $the_query = new WP_Query( array( 'tag_id' => 12 ) );
 
                     if ( $the_query->have_posts() ) {
@@ -43,9 +43,10 @@
                     }
                     /* Restore original Post Data */
                     wp_reset_postdata();
-                ?></div>
+                ?>
+            </div>
         <h1 class="van">VANTAA</h1>    
-        <div class="vpiilo"></div>
+            <div class="vpiilo"></div>
         
         
        
