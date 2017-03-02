384422a393f48f7b82949d8f1d323e5d5e2e5642
“ilkkaper” <ilkkaper@metropolia.fi>
2017-03-02 19:12:40 +0200

    New form for adding posts, map not working yet. small changes in css

--- uusi-template.php
+++ uusi-template.php
@@ -1,85 +1,120 @@
 <?php /* Template Name: uusi-template */ ?>
-<?php get_header(); ?>
-<div class="content-row">
-<body>
-    <div id="map"></div>
-    <div id="form">
-      <table>
-          <!-- tää on kartalle tulevan merkin infowindowin sisälle tuleva formi, jonka pitäis lähettää noi tiedot databaseen (en osannu saada toimia) ja vielä väärät tiedot menee koska säätö-->
-      <tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>
-      <tr><td>Address:</td> <td><input type='text' id='address'/> </td> </tr>
-          <tr><td>Type:</td> <td><select id='type'> +
-                 <option value='bar' SELECTED>bar</option>
-                 <option value='restaurant'>restaurant</option>
-                 </select> </td></tr>
- <tr><td></td><td><input type='button' value='Save' onclick='saveData()'/></td></tr>
-      </table>
 
-    </div>
-    <div id="message">Location saved</div>
+
+<?php
+// Check if the form was submitted
+if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] )) {
  
-    <?php
-    // tää koodi on pluginista, formi joka ottaa kuvan, tägin ja titlen postille. Laittaa kuvan thumbnailiksi koska jos menee contentin sisälle ei näy sivulla ja hauissa
-    if (function_exists('user_submitted_posts')) user_submitted_posts(); ?>
+        // Do some minor form validation to make sure there is content
+        if (isset ($_POST['title'])) {
+                $title =  $_POST['title'];
+        } else {
+                echo 'Please enter a title';
+        }
+        if (isset ($_POST['description'])) {
+                $description = htmlentities(trim(stripcslashes($_POST['description'])));
+    } else {
+        echo 'Please enter the content';
+        }
+ 
+        $tags = $_POST['post_tags'];
+       
+ 
+        // Add the content of the form to $post as an array
+        $type = trim($_POST['Type']);
+        $post = array(
+                'post_title'    => $title,
+                'post_content'  => $description,
+                'post_status'   => 'publish',                     // Choose: pending, preview, future, etc. <- tällä pystyis moderoimaan postauksia, jos laittaa pending niin adminin on hyväksyttävä ennen ku tulee julkiseksi
+                'tags_input'    => array($tags),
+                'tax_input'    => array( $type),
+                'comment_status' => 'closed',
+                'post_author' => '2',
+            
+            
+                
+        );
+        $post_id = wp_insert_post($post);
+    
+        $new_post = wp_insert_post($post_array);
+    
+        if (!function_exists('wp_generate_attachment_metadata')){
+                require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+                require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+                require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+            }
+             if ($_FILES) {
+                foreach ($_FILES as $file => $array) {
+                    if ($_FILES[$file]['error'] !== UPLOAD_ERR_OK) {
+                        return "upload error : " . $_FILES[$file]['error'];
+                    }
+                    $attach_id = media_handle_upload( $file, $new_post );
+                }   
+            }
+            if ($attach_id > 0){
+                //and if you want to set that image as Post  then use:
+                update_post_meta($new_post,'_thumbnail_id',$attach_id);
+            }
 
-
-   
-    <script>
-      var map;
-      var marker;
-      var infowindow;
-      var messagewindow;
+    
+        wp_set_post_terms($post_id,$type,'Type',true);
         
-        //näyttää kartan sivulla
-      function initMap() {
-        var suomi = {lat: 60.8215965, lng: 24.9911299};
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: suomi,
-          zoom: 13
-        });
-          //tekee infosivun johon hakee formin näytettäväksi
-           infowindow = new google.maps.InfoWindow({
-          content: document.getElementById('form')
-        })
+        wp_redirect( home_url('/listing-submitted/') ); // redirect to home page after submit
+        exit();
+ 
+            if ($_FILES) {
+                foreach ($_FILES as $file => $array) {
+                    $newupload = insert_attachment($file,$post_id);
+                    // $newupload returns the attachment id of the file that
+                    // was just uploaded. Do whatever you want with that now.
+                }
+        }
+}
+ // end IF
 
-        messagewindow = new google.maps.InfoWindow({
-          content: document.getElementById('message')
-        });
-          //mahdollistaa klikkauksen merkin kartalle
-        google.maps.event.addListener(map, 'click', function(event) {
-          marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map
-          });
+?>
 
-            //avaa infoikkunan kun klikkaa
-          google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-          });
-        });
-      }
-        //tän pitäis laittaa tiedot databaseen mut ei toimi (pitää tehä erikseen databaseen oma taulu tätä varten) ja tehä joku oma php file missä ne jutut mitkä tajuu et yhdistetään sinne. yritin monta kertaa mutten osannut murr
-function saveData() {
-        var name = escape(document.getElementById('name').value);
-        var address = escape(document.getElementById('address').value);
-        var type = document.getElementById('type').value;
-        var latlng = marker.getPosition();
-          /* ei toimi ne jutut tuolla merkkiuploadissa*/ $.get('merkkiupload.php',{lat:latlng.lat(),lng:latlng.lng(),name:name,adress:address, type:type})
-     .done(function(data){  
-      alert("Data Loaded: " + data);
-     });
-    };
+<?php get_header(); ?>
+  <div id="container">
+                <div id="content" role="main">
+ 
+                        <h1 class="page-title">Lisää katukuva</h1>
+ 
+<!--SUBMIT POST-->
+                        <form id="new_post" name="new_post" class="post_work" method="post" enctype="multipart/form-data">
+                                <p><label for="title">Otsikko</label><br />
+                                        <input type="text" id="title" class="required" value="" tabindex="1" size="20" name="title" />
+                                </p>
+                                <p><label for="description">Kuvaus</label><br />
+                                        <textarea id="description" type="text" class="required" tabindex="3" name="description" cols="50" rows="6"></textarea>
+                                </p>
+ 
+                               
+<!--                            <p><label for="attachment">Photos: </label>
+                                        <input type="file" id="attachment">
+                                        <div id="attachment_list"></div></p>
+-->
+                            
+                                <input type="file" name="thumbnail" id="thumbnail">
+ 
+                                <p>Tägit: <input type="text" value="" tabindex="35" name="post_tags" id="post_tags" /></p>
+                                <input type="hidden" name="post_type" id="post_type" value="domande" />
+                                <input type="hidden" name="action" value="post" />
+ 
+                                <p align="right"><input type="submit" value="Submit" tabindex="6" id="submit" name="submit" /></p>
+ 
+                                <?php wp_nonce_field( 'new-post' ); ?>
+                        </form>
+ 
+                        <script>
+                            var multi_selector = new MultiSelector( document.getElementById( 'attachment_list' ), 8 );
+                            multi_selector.addElement( document.getElementById( 'attachment' ) );
+                        </script>
+ 
+<!--SUBMIT POST END-->
+ 
+                </div><!-- .content -->
+            </div><!-- #container -->
+ 
+<?php get_footer(); ?>
 
-    </script>
-    
-    
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdrDkVOK-e9FVtjG0fr1EzY4gRpU9AsvM&callback=initMap"
-    async defer></script>
-        
-</body>
-
-
-
-    
-
-<?php get_footer();?>
