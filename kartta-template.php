cc9f1fd565ed754f86a51c7d059849b12053ac87
“ilkkaper” <ilkkaper@metropolia.fi>
2017-03-07 15:19:29 +0200

    map template added, and some minor changes

--- kartta-template.php
+++ kartta-template.php
@@ -1,105 +1,15 @@
-<?php /* Template Name: uusi-template */ ?>
+<?php /* Template Name: kartta-template */ ?>
 
 
-<?php
-// Check if the form was submitted
-if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] )) {
- 
-        // Do some minor form validation to make sure there is content
-        if (isset ($_POST['title'])) {
-                $title =  $_POST['title'];
-        } else {
-                echo 'Please enter a title';
-        }
- 
-        $tags = $_POST['post_tags'];
-        $displaylat = $_POST['displayLat'];
-        $displaylong = $_POST['displayLong'];
-        
-
-        // Add the content of the form to $post as an array
-        $type = trim($_POST['Type']);
-        $post = array(
-                'post_title'    => $title,
-                //'post_content'  => $file,
-                'post_status'   => 'publish',                     // Choose: pending, preview, future, etc. <- tällä pystyis moderoimaan postauksia, jos laittaa pending niin adminin on hyväksyttävä ennen ku tulee julkiseksi
-                'tags_input'    => array($tags),
-                'tax_input'    => array( $type),
-                'comment_status' => 'closed',
-                'post_author' => '2',
-                'displayLat'    =>   $displaylat,
-                'displayLong'    =>   $displaylong
-                       
-        );
-    
-        $post_id = wp_insert_post($post);
-  
-        //$new_post = wp_insert_post($post_array);
-    
-        if (!function_exists('wp_generate_attachment_metadata')){
-                require_once(ABSPATH . "wp-admin" . '/includes/image.php');
-                require_once(ABSPATH . "wp-admin" . '/includes/file.php');
-                require_once(ABSPATH . "wp-admin" . '/includes/media.php');
-            }
-             if ($_FILES) {
-                foreach ($_FILES as $file => $array) {
-                    if ($_FILES[$file]['error'] !== UPLOAD_ERR_OK) {
-                        return "upload error : " . $_FILES[$file]['error'];
-                    }
-                    $attach_id = media_handle_upload( $file, $post_id );
-                    //set_post_thumbnail($new_post,$attach_id);
-                }   
-            }
-             if ($attach_id > 0){
-                //and if you want to set that image as Post  then use:
-                update_post_meta($post_id,'_thumbnail_id',$attach_id);
-            }
-
-    
-        wp_set_post_terms($post_id,$type,'Type',true);
-        add_post_meta($post_id, 'metadisplayLat', $displaylat, false);
-        add_post_meta($post_id, 'metadisplayLong', $displaylong, false);
-       
-        
-       
-        
-        wp_redirect( home_url('') ); // redirect to home page after submit
-       
-}
- // end IF
-
-?>
 
 <?php get_header(); ?>
   <div id="container">
                 <div id="content" role="main">
  
-                        <h1 class="page-title">LISÄÄ KATUKUVA</h1>
- 
-<!--SUBMIT POST-->
                     <div id="map"></div>
                     
                     
-                        <form id="new_post" name="new_post" class="post_work" method="post" enctype="multipart/form-data">
-                                <p><label for="title">Otsikko</label><br />
-                                <input type="text" id="title" class="required" value="" tabindex="1" size="20" name="title" />
-                                </p>
-
-                                <input type="file" name="thumbnail" id="thumbnail">
- 
-                                <p>Tägit: <input type="text" value="" tabindex="35" name="post_tags" id="post_tags" /></p>
-                                <input type="hidden" name="post_type" id="post_type" value="domande" />
-                                <input type="hidden" name="action" value="post" />
- 
-                                <p align="right"><input type="submit" value="Submit" tabindex="6" id="submit" name="submit" /></p>
-                            
-                                <input type="hidden" size="20" maxlength="50" name="displayLat" id="displayLat"><br />
-                                <input type="hidden" size="20" maxlength="50" name="displayLong" id="displayLong"><br />
- 
-                                <?php wp_nonce_field( 'new-post' ); ?>
-                        </form>
- 
-                     
+                      
                         <script>
                             
                         var map;
@@ -246,27 +156,15 @@ if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] )) {
                       map = new google.maps.Map(document.getElementById('map'), myOptions);          
       
           
-                                //lisää paikkatiedon formiin, piilotettuihin inputteihin.
-                                google.maps.event.addListener(map, 'click', function(event) {
-                                document.getElementById('displayLat').value = event.latLng.lat();
-                                document.getElementById('displayLong').value = event.latLng.lng(); 
-                                });
- 
-                                //lisää markkerin kartalle
-                                google.maps.event.addListener(map, 'click', function(event) {
-                                    marker = new google.maps.Marker({
-                                        position: event.latLng,
-                                        map: map
-                                        });
-                                });
-                                }
+                               
+                                };
+                                
                         </script>
                     
                     
                     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdrDkVOK-e9FVtjG0fr1EzY4gRpU9AsvM&callback=initMap"async defer></script>
                     
- 
-<!--SUBMIT POST END-->
+
  
                 </div><!-- .content -->
             </div><!-- #container -->
