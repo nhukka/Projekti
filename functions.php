db7f779413ec5cb0fefb1fb379e74c4483ef318d
nhukka <nea.hukka@gmail.com>
2017-03-08 23:21:04 +0200

    trying to put our posts on map with markers

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ function custom_theme_setup(){
     add_theme_support('title-tag');
     add_theme_support('custom-background');
     add_theme_support('custom-header'); 
+    add_theme_support('custom-fields'); 
 }
 add_action('after_setup_theme', 'custom_theme_setup');
 
--- kartta-template.php
+++ kartta-template.php
@@ -10,31 +10,63 @@
         
                     
                      <?php
+                    
+                    //tää $testi hakee kaikki ne postit missä on lisäkentttänä noi displayt monelemmat ja näyttää molemmat arvot 
+    
+                $the_query = new WP_Query( array(
+                        'meta_query' => array(
+                        'relation' => 'AND' 
+                        ),
+                        array(
+                        'key' => 'metadisplayLat',
+                        ),
+                        array(
+                        'key' => 'metadisplayLong',
+                             ),) );
+                    if ( $the_query->have_posts() ) {
+                        while ( $the_query->have_posts() ) {
+                            $the_query->the_post();
+                            echo get_the_title();
+                            echo get_post_meta(get_the_ID(), 'metadisplayLat', true);
+                            echo get_post_meta(get_the_ID(), 'metadisplayLong', true);
+                            
+                        }
+                    } else {
+                        // no posts found
+                    }
+                   
+                    wp_reset_postdata();
+                    
+                    
+           
+            /*    ei toimi tän kanssa toi $testi vaa tää on se mitä ilen ohjeessa oli, en vaan ymmärrä miten noi pilkut tuolla ja pisteet menee ku toi herjaa 
+            
                     $the_query = new WP_Query( array() );
                     if ( $the_query->have_posts() ) {
                         echo '<script>
                         var Koordinaatit = [';
                         while ( $the_query->have_posts() ) {
                             $the_query->the_post();
-                            echo '{lat:'.
-                            get_post_meta($post->ID, 'metadisplayLat', true) .',lng:'. get_post_meta($post->ID, 'metadisplayLong', true).'}';
+                            echo '{lat:'. get_post_meta( $post->ID, 'metadisplayLat') .',lng:'. echo get_post_meta( $post->ID, 'metadisplayLong').'}';
                             
                         }
                         echo ']</script>';
                     } else {
                         // no posts found
                     }
-                    /* Restore original Post Data */
-                    wp_reset_postdata();
-                ?>
+                   
+                    wp_reset_postdata();*/
+                                    ?>
                         <script>
                             
                             
                         var map;
-                        var marker;
+                        //var marker;
                             //näyttää kartan sivulla
                             function initMap() {
-                                var suomi = {lat: 60.230635, lng: 25.404319};
+                            var suomi = {lat: 60.230635, lng: 25.404319};
+                            var myLatLng = {lat: 60.230635, lng: 25.404319};
+
                                 
                            
                         var myOptions = {
@@ -171,19 +203,21 @@
                         }]
                     };
 
-                      map = new google.maps.Map(document.getElementById('map'), myOptions);          
+                      map = new google.maps.Map(document.getElementById('map'), myOptions);
                                 
-                        var Koordinaatit = {
-                        lat: {<?php echo get_post_meta($post->ID, 'metadisplayLat', true) ?>, lng: <?php
-                        echo get_post_meta($post->ID, 'metadisplayLong', true);
-                        ?>
-                        }};
-                         
-      
-          
-                               
-                                };
+                            
+                    };
+                            //testaan vaan et saanko ees yhtä markkerii näkyviin vaan et määrittelen sen sijainnin, ei onnistunu
+                            var marker = new google.maps.Marker({
+                            position: myLatLng,
+                            map: map,
+                        
+  });
+                            
+                            marker.setMap(map);
+
                                 
+                   
                         </script>
                     
                     
